08145ffc64ec68fa115f7021720d644b9fd208a1
HamidiMehdi <mehdi.hamidi.sio@gmail.com>
2020-10-10 17:14:58 +0200

    background music added, error started audio in loading page (error google)

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,57 @@
-let mario = document.querySelector("#mario");
-let secondFloor = document.querySelector("#second-floor");
-let floor = document.querySelector("#floor");
-let landscape = document.querySelector("#landscape");
-let sky = document.querySelector("body");
-
-// Save the shift of elements
-let shift = 0;
-
-window.onload = () => {
-    document.addEventListener("keydown", walk);
-    document.addEventListener("keyup", stopWalking);
+let marioFunc = {
+    mario : document.querySelector("#mario"),
+    secondFloor : document.querySelector("#second-floor"),
+    floor : document.querySelector("#floor"),
+    landscape : document.querySelector("#landscape"),
+    sky : document.querySelector("body"),
+    backgroundMusic: document.querySelector('audio.background-music'),
+    shift : 0,
+    walk: function (event) {
+        switch(event.key){
+            case "ArrowLeft":
+                marioFunc.mario.classList.add("mario-left");
+                marioFunc.mario.classList.remove("mario-right");
+                marioFunc.parallaxe();
+                marioFunc.shift++;
+                break;
+            case "ArrowRight":
+                marioFunc.mario.classList.add("mario-right");
+                marioFunc.mario.classList.remove("mario-left");
+                marioFunc.parallaxe();
+                marioFunc.shift--;
+                break
+        }
+    },
+    stopWalking: function () {
+        marioFunc.mario.classList.remove("mario-left");
+        marioFunc.mario.classList.remove("mario-right");
+    },
+    parallaxe: function () {
+        marioFunc.floor.style.backgroundPositionX = (marioFunc.shift * 4) + "px";
+        marioFunc.secondFloor.style.backgroundPositionX = (marioFunc.shift * 4) + "px";
+        marioFunc.landscape.style.backgroundPositionX = (marioFunc.shift * 2) + "px";
+        marioFunc.sky.style.backgroundPositionX = marioFunc.shift + "px";
+    },
+    startBackgroundMusic: function () {
+        console.log(marioFunc.backgroundMusic);
+        marioFunc.backgroundMusic.play();
+    }
 };
 
-function walk(event){
-    switch(event.key){
-        case "ArrowLeft":
-            mario.classList.add("mario-left");
-            mario.classList.remove("mario-right");
-            parallaxe();
-            shift++;
-            break;
-        case "ArrowRight":
-            mario.classList.add("mario-right");
-            mario.classList.remove("mario-left");
-            parallaxe();
-            shift--;
-            break
-    }
-}
+let marioListener = {
+    onLoad : function () {
+        marioListener.onKey();
+        marioListener.onAudio();
+    },
+    onKey : function () {
+        document.addEventListener("keydown", marioFunc.walk);
+        document.addEventListener("keyup", marioFunc.stopWalking);
+    },
+    onAudio : function () {
+        marioFunc.startBackgroundMusic();
+    },
+};
 
-function stopWalking(){
-    mario.classList.remove("mario-left");
-    mario.classList.remove("mario-right");
-}
-
-function parallaxe(){
-    floor.style.backgroundPositionX = (shift * 4) + "px";
-    secondFloor.style.backgroundPositionX = (shift * 4) + "px";
-    landscape.style.backgroundPositionX = (shift * 2) + "px";
-    sky.style.backgroundPositionX = shift + "px";
-}
+window.onload = () => {
+    marioListener.onLoad();
+};
